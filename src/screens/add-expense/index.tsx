36818aec965543ce32a23a196eb2b81c59954587
Jayesh Kumar <jayeshk118@gmail.com>
2023-07-01 02:32:20 +0530

    route interface created for addExpense screen and save,delete button been added to form

--- src/screens/add-expense/index.tsx
+++ src/screens/add-expense/index.tsx
@@ -1,16 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import PaddingView from '../../common/components/padding-view'
 import ScreenHeader from '../../common/components/screen-header'
 import TypeContainer from './components/TypeContainer'
 import styles from './styles'
-import { useRoute } from '@react-navigation/native'
-import { TransactionInterface } from '../../common/interface'
+import { RouteProp, useRoute } from '@react-navigation/native'
 import Form from './components/Form'
 import moment from 'moment'
 
+interface AddExpenseParams {
+  type: string;
+}
+
+type AddExpenseRouteProp = RouteProp<Record<string, AddExpenseParams>, string>;
+
 const AddExpense = () => {
-  const route = useRoute()
+  const route = useRoute<AddExpenseRouteProp>()
+  const {type} = route?.params
   const [expenseType, setExpenseType] = useState('Expense') // Expense/Income
   const [transactionData, setTransactionData] = useState({
     date : moment().format("yyyy-MM-DD") ,
@@ -34,6 +40,18 @@ const AddExpense = () => {
         </View>
         <View style={styles.formContainer}>
           <Form expenseType={expenseType} TransactionData={transactionData} setTransactionData={setTransactionData}/>
+        </View>
+        <View style={styles.btnContainer}>
+          {
+            type?.toLowerCase() !== 'create' && (
+              <Pressable style={styles.saveBtn}>
+                <Text style={styles.btnTxt}>Delete</Text>
+              </Pressable>
+            )
+          }
+          <Pressable style={styles.saveBtn}>
+            <Text style={styles.btnTxt}>Save</Text>
+          </Pressable>
         </View>
       </PaddingView>
     </>
--- src/screens/add-expense/styles.ts
+++ src/screens/add-expense/styles.ts
@@ -1,5 +1,6 @@
 import {StyleSheet} from "react-native"
 import { scale } from "../../common/utils"
+import colors from "../../common/colors"
 
 export default StyleSheet.create({
     typeContainer: {
@@ -9,4 +10,20 @@ export default StyleSheet.create({
     formContainer: {
         marginVertical: scale(20)
     },
+    saveBtn: {
+        backgroundColor: colors.lightblue,
+        minWidth: scale(80),
+        maxWidth: scale(120),
+        padding: scale(10),
+        alignItems:'center',
+        borderRadius: scale(5),
+        marginHorizontal: scale(10),
+    },
+    btnContainer:{
+        flexDirection: 'row',
+    },
+    btnTxt:{
+        color: colors.white,
+        fontSize: scale(16),
+    }
 })
